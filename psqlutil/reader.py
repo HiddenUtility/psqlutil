9c1f1f063bd5a0c1688ce0b03d8f590a6f43bf59
Hiroki Uchimura <i.will.be.able.to.see.you@gmail.com>
2023-12-08 17:46:24 +0900

    create reader

--- psqlutil/reader.py
+++ psqlutil/reader.py
@@ -4,7 +4,6 @@ Created on Fri Oct  6 05:27:39 2023
 
 @author: nanik
 """
-
 from __future__ import annotations
 from psqlutil.psql import Psql
 import psycopg2
@@ -12,43 +11,60 @@ from pandas import DataFrame
 from psqlutil.connection_information import ConnectioinInfromation
 
 class Reader(Psql):
-    #//Field
-    _info: ConnectioinInfromation
-    querys: list[str] 
-    #@orverride
+    __info: ConnectioinInfromation
+    __querys: list[str] 
+    def __init__(self, info: ConnectioinInfromation=ConnectioinInfromation(), querys: list[str] = []):
+        if not isinstance(info , ConnectioinInfromation): TypeError()
+        if not isinstance(querys , list): TypeError()
+        self.__info = info
+        self.__querys = querys
+
+    # @override
     def __add__(self,obj: Reader) -> Reader:
         if not isinstance(obj, Reader): raise TypeError()
-        self.querys += obj.querys
-        return Reader(self.info ,self.querys)
-    #@orverride
-    def set_free_query(self, *querys: str) -> Reader:
-        for query in querys:
-            if not isinstance(query, str): raise TypeError()
-            if not self._in_query(query, "SELECT"): raise SyntaxError(f"{query}は使用できません。")
-        return self._return(*querys)
+        querys = obj.to_querys() + self.__querys()
+        return Reader(self.__info , querys)
+    
+    # @override
+    def set_querys(self,querys :list[str]) -> Reader:
+        querys = querys + self.__querys()
+        return Reader(self.__info , querys)
+    
+    # @override
+    def to_querys(self):
+        return self.__querys
+
+    # @override
+    def commit(self) -> None:
+        ...
     
     def read(self) -> tuple[list[list[str]], list[str]]:
-        if len(self.querys) == 0: raise Exception("queryがセットされていません。")
-        # connect to PostgreSQL and create table
-        conn = psycopg2.connect(
-            host=self._info.host, 
-            port=self._info.port, 
-            user=self._info.username, 
-            password=self._info.password, 
-            database=self._info.database
-        )
-        cur = conn.cursor()
-        for query in self.querys:
-            cur.execute(query)
-        rows = cur.fetchall()
-        colnames = [col.name for col in cur.description]
-        # close connection
-        cur.close()
-        conn.close()
-        self.querys = []
+        if len(self.__querys) == 0: raise Exception("queryがセットされていません。")
+        try:
+            conn = psycopg2.connect(
+                host=self.__info.host, 
+                port=self.__info.port, 
+                user=self.__info.username, 
+                password=self.__info.password, 
+                database=self.__info.database
+            )
+            cur = conn.cursor()
+        except Exception as e:
+            raise e
+
+        try:
+            for query in self.__querys:
+                cur.execute(query)
+            rows = cur.fetchall()
+            colnames = [col.name for col in cur.description]
+        except Exception as e:
+            raise e
+        finally:
+            cur.close()
+            conn.close()
         return rows, colnames
     
     def get_df(self) -> DataFrame:
-        rows,columns = self.read()
+        rows, columns = self.read()
         if len(rows) == 0:return DataFrame()
         return DataFrame(rows, columns=columns)
