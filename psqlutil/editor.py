66ef564d61da3ac040086c3a4cada72823ce0ce3
Hiroki Uchimura <i.will.be.able.to.see.you@gmail.com>
2023-12-08 17:36:07 +0900

    create editor

--- psqlutil/editor.py
+++ psqlutil/editor.py
@@ -1,36 +1,42 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Sep  1 18:08:17 2023
-
-@author: nanik
-"""
-
-
 from __future__ import annotations
-from psqlutil.creator import Creator
+from psqlutil.psql import Psql
+from psqlutil.committing import Committing
+from psqlutil.connection_information import ConnectioinInfromation
 
 
-class Editor(Creator):
-    #//Field
-    querys: list[str]
-    #@orverride
+class Editor(Psql):
+    __info: ConnectioinInfromation
+    __querys: list[str] 
+    def __init__(self, info: ConnectioinInfromation=ConnectioinInfromation(), querys: list[str] = []):
+        if not isinstance(info , ConnectioinInfromation): TypeError()
+        if not isinstance(querys , list): TypeError()
+        self.__info = info
+        self.__querys = querys
+
+    # @override
     def __add__(self,obj: Editor) -> Editor:
         if not isinstance(obj, Editor): raise TypeError()
-        self.querys += obj.querys
-        return Editor(self.info ,self.querys)
-    #@orverride
-    def set_free_query(self, *querys: str) -> Editor:
-        for query in querys:
-            if not isinstance(query, str): raise TypeError()
-            if not self._in_query(query, "DELETE"): raise SyntaxError(f"{query}は使用できません。")
-            if not self._in_query(query, "UPDATE"): raise SyntaxError(f"{query}は使用できません。")
-            if not self._in_query(query, "INSERT"): raise SyntaxError(f"{query}は使用できません。")
-        return self._return(*querys)
+        querys = obj.to_querys() + self.__querys()
+        return Editor(self.__info , querys)
     
-    def delete_duplicate(self,table_name, *columns: str) -> Editor:
+    # @override
+    def set_querys(self,querys :list[str]) -> Editor:
+        querys = querys + self.__querys()
+        return Editor(self.__info , querys)
+    
+    # @override
+    def to_querys(self):
+        return self.__querys
+
+    # @override
+    def commit(self) -> None:
+        Committing(self.__info, self.__querys).commit()
+    
+    def add_delete_duplicate(self,table_name, *columns: str) -> Editor:
         if len(columns) == 0:return self
         key = ", ".join([f"{column}" for column in columns])
-        andQuery = "WHERE "  + " AND ".join(["t2.{column} = t1.{column}" for column in columns])
+        and_query = "WHERE "  + " AND ".join(["t2.{column} = t1.{column}" for column in columns])
         query = """
         DELETE FROM {0} t1
         WHERE EXISITS(
@@ -39,5 +45,6 @@ class Editor(Creator):
         {2}
         AND t2.ctid > t1.ctid
         );
-        """.format(table_name, key, andQuery)
-        return self._return(query)
+        """.format(table_name, key, and_query)
+        return self.set_querys([query])
+        
